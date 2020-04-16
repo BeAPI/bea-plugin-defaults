6a70a94c5bb23ffe0bdebad60630dedd27592735
Romain DORR <romain.dorr@gmail.com>
2020-04-16 14:42:26 +0200

    Update WP Deffer with new hook

--- default-wp-deffer.php
+++ default-wp-deffer.php
@@ -1,7 +1,7 @@
 <?php
 /*
  Plugin Name: BEA Default - WP Deferred JavaScripts
- Version: 1.0.0
+ Version: 1.1.0
  Plugin URI: https://beapi.fr
  Description: Force to no defer some scripts
  Author: Be API
@@ -9,7 +9,7 @@
 
  ----
 
- Copyright 2018 Be API Technical team (humans@beapi.fr)
+ Copyright 2020 Be API Technical team (humans@beapi.fr)
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -30,9 +30,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Cannot access pages directly.' );
 }
 
-add_filter( 'option_' . 'wdjs_do_not_defer_opt', 'bea_default_do_not_deffer' );
-add_filter( 'default_' . 'wdjs_do_not_defer_opt', 'bea_default_do_not_deffer' );
+add_filter( 'do_not_defer', 'beapi_do_not_deffer' );
+/**
+ * @param $do_not_defer
+ *
+ * @return array
+ *
+ * @author Romain DORR <rdorr@beapi.fr>
+ */
+function beapi_do_not_deffer( $do_not_defer ) {
+	$do_not_defer[] = 'jquery';
+	$do_not_defer[] = 'jQuery';
+	$do_not_defer[] = 'flow-flow-plugin-script';
+	$do_not_defer[] = 'gmap';
+	$do_not_defer[] = 'recette';
+	$do_not_defer[] = 'wpforms';
 
-function bea_default_do_not_deffer() {
-	return [ 'jquery', 'jQuery', 'flow-flow-plugin-script', 'gmap', 'recette' ];
+	return $do_not_defer;
 }
