b20a8a7fc4deeae8bb5e55bbbabae82ef2496654
Amaury Balmer <amaury@beapi.fr>
2024-03-15 21:42:29 +0100

    remove old amp default, add imagify

--- default-imagify.php
+++ default-imagify.php
@@ -1,53 +1,63 @@
 <?php
 /*
-Plugin Name: BEA Default - Accelerated Mobile Pages
+Plugin Name: BEA Default - Imagify
 Version: 1.0.0
 Plugin URI: https://beapi.fr
-Description: Force default options for Accelerated Mobile Pages
+Description: Default optimal settings for Imagify plugin
 Author: Be API
 Author URI: https://beapi.fr
+
 ----
+
 Copyright 2018 Be API Technical team (humans@beapi.fr)
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
+
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 */
 
-namespace BEAPI\Plugin_Defaults\AMP;
+namespace BEAPI\Plugin_Defaults\Imagify;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Cannot access pages directly.' );
 }
 
+add_filter( 'pre_option_imagify_settings', __NAMESPACE__ . '\\default_options' );
+
 /**
- * Set by default AMP metabox to be on "Hide" for posts and pages
+ * Overwrite Imagify default options
  *
- * @author Maxime CULEA
+ * @return int[]
  */
-add_action(
-	'admin_print_footer_scripts',
-	function () {
-		$screen = get_current_screen();
-		if ( empty( $screen ) ) {
-			return;
-		}
-
-		if ( 'add' !== $screen->action || 'post' !== $screen->base ) {
-			return;
-		}
-
-		if ( ! in_array( $screen->post_type, [ 'post', 'page' ], true ) ) {
-			return;
-		}
-
-		echo '<script type="text/javascript">jQuery("#ampforwp-on-off-meta-radio-two").attr("checked",true);</script>';
-	}
-);
+function default_options(): array {
+	return [
+		'api_key'                => '',
+		'optimization_level'     => 2,
+		'lossless'               => 0,
+		'auto_optimize'          => 1,
+		'backup'                 => 1,
+		'resize_larger'          => 1,
+		'resize_larger_w'        => 2560,
+		'display_nextgen'        => 1,
+		'display_nextgen_method' => 'rewrite',
+		'display_webp'           => 1,
+		'display_webp_method'    => 'rewrite',
+		'cdn_url'                => '',
+		'disallowed-sizes'       => [],
+		'admin_bar_menu'         => 0,
+		'partner_links'          => 0,
+		'convert_to_avif'        => 1,
+		'convert_to_webp'        => 1,
+	];
+}
