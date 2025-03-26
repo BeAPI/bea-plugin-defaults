bce77d5f0cb1676a02e823d0360fefb68414d503
julesfell <133788677+julesfell@users.noreply.github.com>
2025-03-26 18:10:21 +0100

    force real IP address logging in Stream plugin, in case of reverse proxy usage

--- default-stream.php
+++ default-stream.php
@@ -11,6 +11,7 @@ namespace BEAPI\Plugin_Defaults\Stream;
 
 add_filter( 'default_site_option_wp_stream_network', __NAMESPACE__ . '\\stream_settings' );
 add_filter( 'default_option_wp_stream', __NAMESPACE__ . '\\stream_settings' );
+add_filter( 'wp_stream_client_ip_address', __NAMESPACE__ . '\\stream_client_ip_address' );
 
 /**
  * Default setting for Stream plugin
@@ -31,4 +32,27 @@ function stream_settings(): array {
 		'advanced_wp_cron_tracking'         => 0,
 		'advanced_delete_all_records'       => 0,
 	];
+}
+
+/**
+ * In case of reverse proxy, we need to log the real IP address
+ *
+ * @param $ip_address
+ *
+ * @return string
+ * @author Jules Fell
+ */
+function stream_client_ip_address( $ip_address ): string {
+	if ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+		$xff = $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+		if ( strpos( $xff, ',' ) ) {
+			$ip_list   = explode( ',', $xff );
+			$ip_address = trim( $ip_list[0] ); // First IP of the list is the original client IP
+		} else {
+			$ip_address = trim( $xff ); // If only one IP in HTTP_X_FORWARDED_FOR
+		}
+	}
+
+	return $ip_address;
 }
