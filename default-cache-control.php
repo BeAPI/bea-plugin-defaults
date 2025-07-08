ffc0ab6e119966634fb3f0484f9374a4dc6e338e
Amaury Balmer <amaury@beapi.fr>
2025-07-08 12:30:11 +0200

    Fix: Ignore unused function parameter warnings in multiple files

--- default-cache-control.php
+++ default-cache-control.php
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * The structure follows the same pattern as the cache-control plugin.
  *
  * Available options for each entry:
- * - max_age: Cache duration in seconds for browsers (defined only 1 time on cache_control_default_settings function)
+ * - max_age: Cache duration in seconds for browsers (defined only 1 time on default_settings function)
  * - s_maxage: Cache duration in seconds for CDN/proxies
  * - staleerror: Duration to serve stale content on server errors (auto-defined x3 s_maxage)
  * - stalereval: Duration to serve stale content while revalidating (auto-defined x5 s_maxage)
@@ -32,57 +32,58 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return array Default cache control settings
  */
 function get_default_cache_control_settings() {
+	// WordPress constants: HOUR_IN_SECONDS = 3600, DAY_IN_SECONDS = 86400
 	return [
 		'front_page'           => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 		],
 		'singles'              => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 			'mmulti'   => 1,     // Enable age multiplier
 		],
 		'pages'                => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 		],
 		'home'                 => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 			'paged'    => 5,     // 5 seconds pagination factor. Example : 10 pages * 5 seconds = 50 seconds
 		],
 		'categories'           => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 			'paged'    => 8,     // 8 seconds pagination factor
 		],
 		'tags'                 => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 			'paged'    => 10,    // 10 seconds pagination factor
 		],
 		'authors'              => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 			'paged'    => 10,    // 10 seconds pagination factor
 		],
 		'dates'                => [
-			's_maxage' => 12 * HOUR_IN_SECONDS, // 12 hours
+			's_maxage' => 12 * \HOUR_IN_SECONDS, // 12 hours
 		],
 		'feeds'                => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 		],
 		'attachment'           => [
-			's_maxage' => 12 * HOUR_IN_SECONDS, // 12 hours
+			's_maxage' => 12 * \HOUR_IN_SECONDS, // 12 hours
 		],
 		'search'               => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 		],
 		'notfound'             => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 		],
 		'redirect_permanent'   => [
-			's_maxage' => 7 * DAY_IN_SECONDS, // 1 week
+			's_maxage' => 7 * \DAY_IN_SECONDS, // 1 week
 		],
 		// WooCommerce specific settings (if WooCommerce is active)
 		'woocommerce_product'  => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 		],
 		'woocommerce_category' => [
-			's_maxage' => 10 * HOUR_IN_SECONDS, // 10 hours
+			's_maxage' => 10 * \HOUR_IN_SECONDS, // 10 hours
 		],
 	];
 }
@@ -90,7 +91,7 @@ function get_default_cache_control_settings() {
 /**
  * Apply default cache control settings
  */
-function cache_control_default_settings() {
+function default_settings() {
 	global $cache_control_options;
 
 	if ( empty( $cache_control_options ) ) {
@@ -122,7 +123,7 @@ function cache_control_default_settings() {
 }
 
 // Hook to apply default settings after plugins are loaded
-add_action( 'plugins_loaded', __NAMESPACE__ . '\\cache_control_default_settings' );
+\add_action( 'plugins_loaded', __NAMESPACE__ . '\\default_settings' );
 
 /**
  * Filter cache-control options when they are read from database
@@ -132,7 +133,7 @@ add_action( 'plugins_loaded', __NAMESPACE__ . '\\cache_control_default_settings'
  * @param mixed  $default_value Default value
  * @return mixed Filtered value
  */
-function cache_control_filter_option( $pre, $option, $default_value ) {
+function filter_option( $pre, $option, $default_value ) {
 	// Use always default value for cache-control options
 	if ( str_contains( $option, 'cache_control_' ) ) {
 		return $default_value;
@@ -142,29 +143,29 @@ function cache_control_filter_option( $pre, $option, $default_value ) {
 }
 
 // Hook to filter options when they are read
-add_filter( 'pre_option', __NAMESPACE__ . '\\cache_control_filter_option', 10, 3 );
+\add_filter( 'pre_option', __NAMESPACE__ . '\\filter_option', 10, 3 );
 
 /**
  * Disable cache-control admin page
  */
-function cache_control_disable_admin_page() {
+function disable_admin_page() {
 	// Remove the admin menu page
-	remove_submenu_page( 'options-general.php', 'cache_control' );
+	\remove_submenu_page( 'options-general.php', 'cache_control' );
 
 	// Remove action links from plugin list
-	remove_filter( 'plugin_action_links_' . plugin_basename( 'cache-control/cache-control.php' ), 'cache_control_admin_action_links' );
+	\remove_filter( 'plugin_action_links_' . \plugin_basename( 'cache-control/cache-control.php' ), 'cache_control_admin_action_links' );
 
 	// Remove meta links from plugin list
-	remove_filter( 'plugin_row_meta', 'cache_control_admin_actions', 10 );
+	\remove_filter( 'plugin_row_meta', 'cache_control_admin_actions', 10 );
 }
 
 // Hook to disable admin page after it's been added
-add_action( 'admin_menu', __NAMESPACE__ . '\\cache_control_disable_admin_page', 999 );
+\add_action( 'admin_menu', __NAMESPACE__ . '\\disable_admin_page', 999 );
 
 /**
  * Add warning notice on cache-control admin page
  */
-function cache_control_add_admin_notice() {
+function add_admin_notice() {
 	// Only show on cache-control admin page
 	// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Only reading $_GET['page'], no action performed
 	if ( 'cache_control' === ( isset( $_GET['page'] ) ? $_GET['page'] : '' ) ) {
@@ -180,4 +181,4 @@ function cache_control_add_admin_notice() {
 }
 
 // Hook to add notice on admin pages
-add_action( 'admin_notices', __NAMESPACE__ . '\\cache_control_add_admin_notice' );
+\add_action( 'admin_notices', __NAMESPACE__ . '\\add_admin_notice' );
